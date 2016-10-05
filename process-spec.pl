f9531104e031b8d993baea5b962a9e5a9595dfc5
matejcik <jmatejek@suse.com>
2016-10-05 17:10:33 +0200

    remove noarch condition when processing

--- process-spec.pl
+++ process-spec.pl
@@ -12,10 +12,24 @@ $SOURCE eq $DEST and $DEST .= ".converted";
 open (SOURCE, "<$SOURCE");
 open (DEST, ">$DEST");
 
+my $oldsuse = 0;
+my $pysub_found = 0;
+
 while (<SOURCE>) {
-    if (/^BuildRequires:(\s+)python-(.*)$/) {
+    if ($oldsuse == 1) {
+        if (/^%else$/) {
+            $oldsuse = 2;
+        } elsif (/^%endif$/) {
+            $oldsuse = 0;
+        }
+        # else nothing
+    } elsif ($oldsuse == 2 && /^%endif$/) {
+        $oldsuse = 0;
+    } elsif ($oldsuse == 2 && /^%else$/) {
+        $oldsuse = 1;
+    } elsif (/^BuildRequires:(\s+)python-(.*)$/) {
         print DEST "BuildRequires:$1%{python_module $2}\n";
-    } elsif (/^%prep/) {
+    } elsif (/^%prep/ && !$pysub_found) {
         print DEST "%python_subpackages\n\n";
         print DEST $_;
     } elsif (/^python setup\.py build/) {
@@ -26,6 +40,13 @@ while (<SOURCE>) {
         print DEST "%python_exec $1\n";
     } elsif (/%\{!\?python_site/) {
         # pass
+    } elsif (/^%if 0%\{\?suse_version\} && 0%\{\?suse_version\} <= 1110$/) {
+        $oldsuse = 1;
+    } elsif (/^%if 0%\{\?suse_version\} > 1110$/) {
+        $oldsuse = 2;
+    } elsif (/^%python_subpackages/) {
+        $pysub_found = 1;
+        print DEST $_;
     } else {
         print DEST $_;
     }
