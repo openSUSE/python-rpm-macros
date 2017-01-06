99ef782b803194b9d7b765b562840a793eb4c7de
matejcik <jmatejek@suse.com>
2017-01-06 18:38:34 +0100

    drop %python_alternative_man

--- macros.lua
+++ macros.lua
@@ -35,8 +35,7 @@ function _python_scan_spec()
 
     function replace_macros(str, targetflavor)
         local LONG_MACROS = { "sitelib", "sitearch",
-            "alternative", "alternative_man",
-            "install_alternative", "uninstall_alternative",
+            "alternative", "install_alternative", "uninstall_alternative",
             "version", "version_nodots", "bin_suffix"}
         local SHORT_MACROS = { "ver" }
         for _, srcflavor in ipairs({flavor, "python"}) do
